cba4b3e3cf04fb8340c578a552bc1b1afb95c63b
Shellbaka <Shellbaka69@gmail.com>
2025-05-02 18:29:51 -0300

    Criar transação é o nome da página da vez, fiz algumas instalações independentes de algumas dependências mas andam apresentando alguns erros que ainda não sei resolver mas irei dar uma atenção especial para.

---
+++ src/app/Components/criar-transacao/criar-transacao.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-transacao-form',
+  templateUrl: './criar-transacao-form.component.html',
+  styleUrls: ['./criar-transacao-form.component.css']
+})
+export class CriarTransacaoFormComponent {}
---
+++ src/app/Pages/criartransacao/criartransacao.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-transacao',
+  templateUrl: './criar-transacao.page.html',
+  styleUrls: ['./criar-transacao.page.css']
+})
+export class CriarTransacaoPage {}
